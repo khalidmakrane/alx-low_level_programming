6cad1035b4c219d2f5ec01fe64d4f07be91f222c
khalidmakrane <khalidmakrane1@gmail.com>
2023-03-16 14:22:40 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts fnction
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of a int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+	return (0);
+}
